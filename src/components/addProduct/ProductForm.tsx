3f3b8df4051bcb5c614c60c578c4fcdfca7441b8
naimuddin94 <developernaimuddin@gmail.com>
2023-12-14 16:20:42 +0600

    fix typescript image error

--- src/components/addProduct/ProductForm.tsx
+++ src/components/addProduct/ProductForm.tsx
@@ -30,26 +30,28 @@ const ProductForm = () => {
     const { images, features: stringFeatures, ...reamingData } = data;
     const features = stringFeatures.split(",");
 
-    const file = images[0];
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", "pntxhvfd");
+    const file = Array.isArray(images) && images.length > 0 ? images[0] : null;
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "pntxhvfd");
 
-    try {
-      const response = await fetch(
-        `https://api.cloudinary.com/v1_1/dxoncladp/image/upload`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      const data = await response.json();
-      console.log({ image: data.secure_url });
-    } catch (error) {
-      console.error("Error uploading image:", error);
+      try {
+        const response = await fetch(
+          `https://api.cloudinary.com/v1_1/dxoncladp/image/upload`,
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+        const data = await response.json();
+        console.log({ image: data.secure_url });
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+
+      reset();
     }
-
-    reset();
   };
 
   type FieldName = keyof Inputs;
