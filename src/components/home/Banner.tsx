50f237de4672d24879690ae6a2c7649a2358a870
naimuddin94 <developernaimuddin@gmail.com>
2023-12-19 01:09:07 +0600

    swiper slider initialization

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,17 +1,36 @@
+"use client";
 import Image from "next/image";
-import cpu from "/public/assets/hyperpc-perfrormance-page-banner.png"
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Banner = () => {
   return (
-    <div className="bg-custom-background bg-cover bg-center min-h-[calc(100vh-64px)] flex items-center text-white">
+    <div className="bg-custom-background bg-cover bg-center min-h-[calc(100vh-64px)]">
+      <div className="w-64 h-64 rounded-full absolute bg-gradient-to-tr from-theme-color-300 -top-32 -left-32"></div>
       <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia vitae
-        saepe veritatis consectetur, quia labore blanditiis dignissimos
-        deserunt, debitis alias placeat, recusandae eaque eveniet nam totam
-        voluptas sint nesciunt quae.
-      </div>
-      <div>
-        <Image src={cpu} alt="banner image" width={400} height={400} />
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
